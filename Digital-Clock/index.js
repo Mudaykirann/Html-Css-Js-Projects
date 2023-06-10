be14c9ef7315ac37372e227897d6603f526744eb
udaykiran1012 <123919244+udaykiran1012@users.noreply.github.com>
2023-06-10 11:11:02 +0530

    project using html css js

---
+++ Digital-Clock/index.js
@@ -0,0 +1,20 @@
+var hours = document.querySelector('.hours');
+var minutes = document.querySelector('.minutes');
+var seconds = document.querySelector('.seconds');
+
+setInterval(showTime,1000);
+function showTime(){
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    hours.innerHTML = hour;
+    minutes.innerHTML = min;
+    seconds.innerHTML = sec;
+}
+showTime();
